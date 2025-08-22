257a729f91744075a40bc382bdc65666c30c9b66
Diretorespecial <diretorespecial@graficaggs.com.br>
2025-08-22 14:32:41 -0300

    Update main.js

--- GraficaGGS 2 - cópia/js/main.js
+++ GraficaGGS 2 - cópia/js/main.js
@@ -53,9 +53,9 @@ $(document).ready(function () {
     let valid = true;
 
     // limpar estados antigos
-    $("input[required], select[required]").removeClass("campo-invalido");
+    $("input[required], select[required], textarea[required]").removeClass("campo-invalido");
 
-    $("input[required], select[required]").each(function () {
+    $("input[required], select[required], textarea[required]").each(function () {
       if ($(this).val().trim() === "") {
         $(this).addClass("campo-invalido"); // adiciona borda vermelha
         valid = false;
@@ -73,17 +73,105 @@ $(document).ready(function () {
   // Funções para PDFs
   // -------------------------
   function gerarReceituario() {
-    alert("Receituário gerado!");
-    // Aqui entra sua lógica de PDF com jsPDF
+    const { jsPDF } = window.jspdf;
+    const doc = new jsPDF();
+
+    doc.setFont("helvetica", "normal");
+    doc.setFontSize(12);
+
+    let tipoPessoa = $("#tipoPessoa").val();
+    let nome, cpfCnpj, endereco;
+
+    if (tipoPessoa === "pf") {
+      nome = $("#nomePF").val();
+      cpfCnpj = $("#cpf").val();
+      endereco = $("#enderecoPF").val();
+    } else {
+      nome = $("#nomePJ").val();
+      cpfCnpj = $("#cnpj").val();
+      endereco = $("#enderecoPJ").val();
+    }
+
+    let quantidade = $("#quantidade").val();
+    let tipoReceituario = $("#tipoReceituario").val() || "Não selecionado";
+
+    doc.text("Receituário", 105, 20, { align: "center" });
+    doc.text("Nome: " + nome, 20, 40);
+    doc.text("CPF/CNPJ: " + cpfCnpj, 20, 50);
+    doc.text("Endereço: " + endereco, 20, 60);
+    doc.text("Tipo de Receituário: " + tipoReceituario, 20, 70);
+    doc.text("Quantidade: " + quantidade, 20, 80);
+
+    doc.save("receituario.pdf");
   }
 
   function gerarProcuracao() {
-    alert("Procuração gerada!");
-    // Aqui entra sua lógica de PDF com jsPDF
+    const { jsPDF } = window.jspdf;
+    const doc = new jsPDF();
+
+    doc.setFont("times", "normal");
+    doc.setFontSize(12);
+
+    let tipoPessoa = $("#tipoPessoa").val();
+    let nome, cpfCnpj, endereco;
+
+    if (tipoPessoa === "pf") {
+      nome = $("#nomePF").val();
+      cpfCnpj = $("#cpf").val();
+      endereco = $("#enderecoPF").val();
+    } else {
+      nome = $("#nomePJ").val();
+      cpfCnpj = $("#cnpj").val();
+      endereco = $("#enderecoPJ").val();
+    }
+
+    doc.text("PROCURAÇÃO", 105, 20, { align: "center" });
+    doc.text(
+      "Eu, " +
+        nome +
+        ", inscrito sob o CPF/CNPJ " +
+        cpfCnpj +
+        ", residente/endereço " +
+        endereco +
+        ", nomeio e constituo meu bastante procurador para os devidos fins.",
+      20,
+      40,
+      { maxWidth: 170 }
+    );
+
+    doc.text("Local e Data: ___________________________", 20, 100);
+    doc.text("Assinatura: ___________________________", 20, 120);
+
+    doc.save("procuracao.pdf");
   }
 
   function gerarRequisicao() {
-    alert("Requisição gerada!");
-    // Aqui entra sua lógica de PDF com jsPDF
+    const { jsPDF } = window.jspdf;
+    const doc = new jsPDF();
+
+    doc.setFont("courier", "normal");
+    doc.setFontSize(12);
+
+    let tipoPessoa = $("#tipoPessoa").val();
+    let nome, cpfCnpj, endereco;
+
+    if (tipoPessoa === "pf") {
+      nome = $("#nomePF").val();
+      cpfCnpj = $("#cpf").val();
+      endereco = $("#enderecoPF").val();
+    } else {
+      nome = $("#nomePJ").val();
+      cpfCnpj = $("#cnpj").val();
+      endereco = $("#enderecoPJ").val();
+    }
+
+    doc.text("REQUISIÇÃO", 105, 20, { align: "center" });
+    doc.text("Solicitante: " + nome, 20, 40);
+    doc.text("CPF/CNPJ: " + cpfCnpj, 20, 50);
+    doc.text("Endereço: " + endereco, 20, 60);
+    doc.text("Descrição da requisição:", 20, 80);
+    doc.text($("#descricao").val(), 20, 90, { maxWidth: 170 });
+
+    doc.save("requisicao.pdf");
   }
 });
